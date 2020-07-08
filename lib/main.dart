0cb747aff44e9bd378c612d491a62c19c71fe68d
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2020-07-08 10:47:10 -0300

    Sessao 8 - Aula 79

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,55 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+//class BallPage extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(
+//        title: Text('Ask Me Anything'),
+//        backgroundColor: Colors.blue[900],
+//      ),
+//      body: Container(color: Colors.blue),
+//    );
+//  }
+//}
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Center(
+        child: Container(
+          color: Colors.blue,
+          height: double.infinity,
+          child: FlatButton(
+            child: Image.asset('images/ball$ballNumber.png'),
+            onPressed: () {
+              setState(() {
+                ballNumber = Random().nextInt(5) + 1;
+              });
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
